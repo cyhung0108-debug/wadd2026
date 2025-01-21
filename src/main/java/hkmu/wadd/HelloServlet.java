861928cec3131e21ae255482614ad002f93c5019
cskeith <cslklee@gmail.com>
2025-01-21 16:55:55 +0800

    Lecture 2 project

---
+++ src/main/java/hkmu/wadd/HelloServlet.java
@@ -0,0 +1,31 @@
+package hkmu.wadd;
+
+import jakarta.servlet.http.*;
+
+import java.io.*;
+import java.util.Date;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = response.getWriter();
+        Date today = new Date();
+        out.println("<!DOCTYPE html><html><body>");
+        out.println("<h1>It is now: " + today + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void init() {
+        System.out.println("Servlet " + this.getServletName() + " has started.");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet " + this.getServletName() + " has ended.");
+    }
+}
