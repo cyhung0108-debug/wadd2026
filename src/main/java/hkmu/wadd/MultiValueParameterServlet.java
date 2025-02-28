1e7a564221b3f8d056823a6db3502461659e6fa2
cskeith <cslklee@gmail.com>
2025-02-28 13:12:18 +0800

    Lab 8 project

---
+++ src/main/java/hkmu/wadd/MultiValueParameterServlet.java
@@ -0,0 +1,30 @@
+package hkmu.wadd;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "multiValueParameterServlet",
+        urlPatterns = {"/checkboxes"})
+public class MultiValueParameterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        RequestDispatcher view
+                = request.getRequestDispatcher("/WEB-INF/jsp/MultiValueForm.jsp");
+        view.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        RequestDispatcher view
+                = request.getRequestDispatcher("/WEB-INF/jsp/MultiValueResult.jsp");
+        view.forward(request, response);
+    }
+}
