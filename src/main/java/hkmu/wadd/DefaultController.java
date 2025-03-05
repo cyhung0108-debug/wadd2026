35437e417904782721e52ecb8ee50d8d9e3fb166
cskeith <cslklee@gmail.com>
2025-03-05 14:19:05 +0800

    Lecture 7 HelloSpringMVC project

---
+++ src/main/java/hkmu/wadd/DefaultController.java
@@ -0,0 +1,38 @@
+package hkmu.wadd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String index() {
+        return "myindexstatic";
+    }
+
+    @GetMapping({"/now", "/time"})
+    public String showTime() {
+        return "currentTime";
+    }
+
+    @GetMapping("/dynamic")
+    public String dynamicindex(ModelMap map) {
+        map.addAttribute("hello", "Welcome to our Spring Lecture !");
+        return "myindex";
+    }
+
+    @GetMapping("/myform")
+    public ModelAndView myform() {
+        return new ModelAndView("myform", "myData", new MyData());
+    }
+
+    @PostMapping("/formhandle")
+    public String formHandle(MyData mydata, ModelMap map) {
+        map.addAttribute("data", mydata);
+        return "myoutput";
+    }
+}
