3e3ce6d7782c9dd1195d8f7fa0dd45706b50e01c
cskeith <cslklee@gmail.com>
2025-03-05 13:47:35 +0800

    Lecture 7 HelloSpringBoot project

---
+++ src/main/java/hkmu/wadd/DefaultController.java
@@ -0,0 +1,18 @@
+package hkmu.wadd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String index() {
+        return "myindexstatic";
+    }
+
+    @GetMapping({"/now", "/time"})
+    public String showTime() {
+        return "currentTime";
+    }
+}
