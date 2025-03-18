125f05acf2111e6d59269758e6dbf5f80bec0101
cskeith <cslklee@gmail.com>
2025-03-18 18:36:14 +0800

    Lecture 8 HelloSpringHibernate project

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO guestbook (date, message, name)
+    VALUES ('2025-03-20 08:39:01.629000', 'Hello', 'Keith');
+INSERT INTO guestbook (date, message, name)
+    VALUES ('2025-03-20 08:39:33.152000', 'Hi', 'John');
