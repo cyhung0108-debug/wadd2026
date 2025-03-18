5b074988aaab60878acf9f30581b266a9bb5d271
cskeith <cslklee@gmail.com>
2025-03-18 15:42:06 +0800

    A new Spring Boot project

---
+++ src/main/java/hkmu/wadd/HelloSpringHibernateApplication.java
@@ -0,0 +1,13 @@
+package hkmu.wadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringHibernateApplication.class, args);
+    }
+
+}
