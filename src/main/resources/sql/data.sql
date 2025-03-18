44f83dfacc203d24c03dae31bed5487323782494
cskeith <cslklee@gmail.com>
2025-03-18 18:51:19 +0800

    Lecture 8 HelloSpringDataJPA project

--- src/main/resources/sql/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO guestbook (date, message, name)
-    VALUES ('2025-03-20 08:39:01.629000', 'Hello', 'Keith');
-INSERT INTO guestbook (date, message, name)
-    VALUES ('2025-03-20 08:39:33.152000', 'Hi', 'John');
