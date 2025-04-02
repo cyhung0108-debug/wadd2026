3de66cf43d9c687bd744d2f08f544550852b5ebe
cskeith <cslklee@gmail.com>
2025-04-02 14:42:13 +0800

    A new Spring Boot project

---
+++ src/main/java/hkmu/wadd/Lecture10Application.java
@@ -0,0 +1,13 @@
+package hkmu.wadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture10Application.class, args);
+	}
+
+}
