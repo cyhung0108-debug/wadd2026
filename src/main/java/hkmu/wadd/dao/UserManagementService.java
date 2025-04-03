d6e0aa9d5dff1f717273d42e805daca499fdcb8a
cskeith <cslklee@gmail.com>
2025-04-03 23:12:13 +0800

    Lab 13 Task 3 completed

--- src/main/java/hkmu/wadd/dao/UserManagementService.java
+++ src/main/java/hkmu/wadd/dao/UserManagementService.java
@@ -1,8 +1,11 @@
 package hkmu.wadd.dao;
 
 import hkmu.wadd.model.TicketUser;
+import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +13,9 @@ import java.util.List;
 
 @Service
 public class UserManagementService {
+    @Autowired
+    private PasswordEncoder pe;
+
     @Resource
     private TicketUserRepository tuRepo;
 
@@ -29,7 +35,17 @@ public class UserManagementService {
 
     @Transactional
     public void createTicketUser(String username, String password, String[] roles) {
-        TicketUser user = new TicketUser(username, password, roles);
+        TicketUser user = new TicketUser(username, pe.encode(password), roles);
         tuRepo.save(user);
+    }
+
+    @Transactional
+    @PostConstruct
+    public void createFirstAdmin() {
+        if (tuRepo.count() == 0) {
+            TicketUser user = new TicketUser("keith", pe.encode("keithpw"),
+                    new String[]{"ROLE_ADMIN", "ROLE_USER"});
+            tuRepo.save(user);
+        }
     }
 }
